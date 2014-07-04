56759118db8c573e406f60fa7f410eab17c997cf
Brandon <bpear96@gmail.com>
2014-07-04 18:32:26 -0400

    Detect android version number.

--- Mako Modem/src/main/java/com/bpear/makomodem/EasyActivity.java
+++ Mako Modem/src/main/java/com/bpear/makomodem/EasyActivity.java
@@ -2,16 +2,24 @@ package com.bpear.makomodem;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EasyActivity extends Activity {
+
+    TextView versionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy);
+        versionView=new TextView(this);
+        versionView=(TextView)findViewById(R.id.versionView);
+        versionView.setText("Android version detected: " + Build.VERSION.RELEASE);
     }
 
 
@@ -24,14 +32,15 @@ public class EasyActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_advanced) {
-            Intent advanced = new Intent(EasyActivity.this,AdvancedActivity.class);
-            startActivity(advanced);
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_advanced:
+                Intent advanced = new Intent(EasyActivity.this,AdvancedActivity.class);
+                startActivity(advanced);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
+
 }
